9bd45509ec66b9d18a2d40fd9a9d65db410d04d2
Sebastian <sebastian.goenaga@hotmail.com>
2019-08-30 23:49:51 -0500

    Terminated

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import {TodoList} from './TodoList.js'
+import { TodoList } from './TodoList.js';
 
 export class TodoApp extends React.Component {
+
   constructor(props) {
     super(props);
     this.state = { items: [], text: '' };
@@ -11,37 +12,63 @@ export class TodoApp extends React.Component {
 
   render() {
     return (
-      <div>
-        <h3>TODO</h3>
-        <TodoList items={this.state.items} />
-        <form onSubmit={this.handleSubmit}>
-          <label htmlFor="new-todo">
-            What needs to be done?
-          </label>
-          <input
-            id="new-todo"
-            onChange={this.handleChange}
-            value={this.state.text}
-          />
-          <button>
-            Add #{this.state.items.length + 1}
-          </button>
-        </form>
-      </div>
+        <div>
+          <h3 align="center">TODO</h3>
+          <TodoList items={this.state.items} />
+          <form onSubmit={this.handleSubmit}>
+            <label htmlFor="todo">
+              ¿Qué necesitas recordar?
+            </label>
+            <input
+                type="text"
+                id="txt"
+                onChange={this.handleChange}
+                value={this.state.text}
+            />
+            <label htmlFor="priority">
+              Prioridad
+            </label>
+            <input
+                type="number"
+                id="n"
+                onChange={this.handleChange}
+                value={this.state.priority}
+            />
+            <label htmlFor="date">
+              ¿Cuándo lo harás?
+            </label>
+            <input
+                type="date"
+                id="d"
+                onChange={this.handleChange}
+                value={this.state.dueDate}
+            />
+            <button>
+              Agregar tarea #{this.state.items.length + 1}
+            </button>
+          </form>
+        </div>
     );
   }
 
   handleChange(e) {
-    this.setState({ text: e.target.value });
+    this.setState({ text: document.getElementById('txt').value })
+    this.setState({ priority: document.getElementById('n').value });;
+    this.setState({ dueDate: document.getElementById('d').value });;
   }
 
   handleSubmit(e) {
+
     e.preventDefault();
+
     if (!this.state.text.length) {
       return;
     }
+
     const newItem = {
       text: this.state.text,
+      priority: this.state.priority,
+      dueDate: this.state.dueDate,
       id: Date.now()
     };
     this.setState(prevState => ({
@@ -49,5 +76,5 @@ export class TodoApp extends React.Component {
       text: ''
     }));
   }
-}
 
+}
